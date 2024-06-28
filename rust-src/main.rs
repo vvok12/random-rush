0c3b9a21aa8a32df3ee9e71f5653ecd100c1f31d
Volodymyr Bezshtanko <v.v.bezshtanko@gmail.com>
2024-06-28 10:03:59 +0300

    simple broadcast funtionality

--- rust-src/main.rs
+++ rust-src/main.rs
@@ -1,16 +1,88 @@
+use std::sync::Arc;
 use axum::{
-    routing::get,
-    Router,
+    extract::{
+        ws::{Message, WebSocket}, 
+        State, 
+        WebSocketUpgrade
+    },
+    response::Response, 
+    routing::get, 
+    Router
+};
+use tokio::sync::{broadcast::{self, Receiver, Sender}, Mutex};
+use futures_util::{
+    stream::{SplitSink, SplitStream},
+    SinkExt, StreamExt,
 };
 
 mod api;
 
+#[derive(Debug, Clone)]
+struct AppState {
+    broadcast_tx: Arc<Mutex<Sender<Message>>>,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let (tx, _) = broadcast::channel(32);
+    let app = AppState {
+        broadcast_tx: Arc::new(Mutex::new(tx)),
+    };
+    
+    let app = Router::new()
+        .route("/", get(|| async { "use /playroom-ws endpoint to connect to web socket" }))
+        .route("/playroom-ws", get(handler)).with_state(app);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler(
+    ws: WebSocketUpgrade, 
+    State(app): State<AppState>,
+) -> Response {
+    ws.on_upgrade(|socket| handle_socket(socket, app))
+}
+
+async fn handle_socket(
+    mut ws: WebSocket, 
+    app: AppState,
+) {
+    let (ws_tx, ws_rx) = ws.split();
+    let ws_tx = Arc::new(Mutex::new(ws_tx));
+
+    {
+        let broadcast_rx = app.broadcast_tx.lock().await.subscribe();
+        tokio::spawn(async move {
+            recv_broadcast(ws_tx, broadcast_rx).await;
+        });
+    }
+
+    recv_from_client(ws_rx, app.broadcast_tx).await;
+}
+
+async fn recv_broadcast(
+    client_tx: Arc<Mutex<SplitSink<WebSocket, Message>>>,
+    mut broadcast_rx: Receiver<Message>,
+) {
+    while let Ok(msg) = broadcast_rx.recv().await {
+        if client_tx.lock().await.send(msg).await.is_err() {
+            return; // disconnected.
+        }
+    }
+}
+
+async fn recv_from_client(
+    mut client_rx: SplitStream<WebSocket>,
+    broadcast_tx: Arc<Mutex<Sender<Message>>>,
+) {
+    while let Some(Ok(msg)) = client_rx.next().await {
+        if matches!(msg, Message::Close(_)) {
+            return;
+        }
+        if broadcast_tx.lock().await.send(msg).is_err() {
+            println!("Failed to broadcast a message");
+        }
+    }
 }
